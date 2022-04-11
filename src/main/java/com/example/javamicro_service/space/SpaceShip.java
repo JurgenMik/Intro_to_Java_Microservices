9ab83dc1b31cdc2290ace3580f34db5bf9ddec40
Jürgen Mikk <jurgen.mikk@khk.ee>
2022-04-11 21:13:00 +0300

    Obj constructor

---
+++ src/main/java/com/example/javamicro_service/space/SpaceShip.java
@@ -0,0 +1,12 @@
+package com.example.javamicro_service.space;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class SpaceShip {
+
+    private String captain;
+    private int fuel;
+}
