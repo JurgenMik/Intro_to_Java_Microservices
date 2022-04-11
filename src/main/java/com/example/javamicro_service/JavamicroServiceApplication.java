afd35cdb5aa85896d705fdbe9ca628ebbe72311e
Jürgen Mikk <jurgen.mikk@khk.ee>
2022-04-11 21:12:18 +0300

    Spring boot/project configuration & pom.xml dependencies

---
+++ src/main/java/com/example/javamicro_service/JavamicroServiceApplication.java
@@ -0,0 +1,14 @@
+package com.example.javamicro_service;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavamicroServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavamicroServiceApplication.class, args);
+    }
+
+}
